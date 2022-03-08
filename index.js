233abc03ead030960ae433804b0414eae81786d3
Stephen Jones <stephen-jones@hotmail.co.uk>
2022-03-08 13:30:56 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const ytdl = require('ytdl-core');
+const app = express();
+const path = require('path');
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(4000, () => {
+    console.log('Server Works !!! At port 4000');
+});
+
+app.get('/download', (req,res) => {
+    var URL = req.query.URL;
+    res.header('Content-Disposition', 'attachment; filename="video.mp3"');
+    ytdl(URL, {
+        format: 'mp3'
+    }).pipe(res);
+});
