4ed88d11abbb1380b6ca8ab50a611c8fd3fe4997
Stephen Jones <stephen-jones@hotmail.co.uk>
2022-03-08 13:51:57 +0000

    updated port

--- index.js
+++ index.js
@@ -6,8 +6,9 @@ const path = require('path');
 
 
 app.use(express.static(path.join(__dirname, 'public')));
+const PORT = process.env.PORT || 4000;
 
-app.listen(4000, () => {
+app.listen(PORT, () => {
     console.log('Server Works !!! At port 4000');
 });
 
